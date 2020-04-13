dac8aad3f66095e16ef20e0b3189a28063c14201
henry <q355261@gmail.com>
2020-04-13 15:50:51 +0800

    test push selenium 3

--- henry_selenium.py
+++ henry_selenium.py
@@ -10,5 +10,19 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options= chrome_options)
 
-driver.get("https://www.104.com.tw/job/5rvmg?jobsource=jolist_b_relevance")
-print(driver.page_source)
+url = "https://fuli.gamer.com.tw/shop.php"
+
+driver.get(url)
+#selenium-使用 https://jzchangmark.wordpress.com/2015/03/16/selenium-%E4%BD%BF%E7%94%A8-css-locator-%E5%AE%9A%E4%BD%8D%E5%85%83%E4%BB%B6/
+titles = driver.find_elements_by_class_name('items-title')
+hrefs = driver.find_elements_by_css_selector("a[class=items-card][href^='https']")#找尋tag為a,class為items-card,href的開頭為https
+imgs = driver.find_elements_by_css_selector("div.card-left.flex-center > img")
+Popularitys = driver.find_elements_by_css_selector("div.card-right div.items-instructions")
+
+listall1 = []
+for i in range(len(titles)):
+    x,y,z=0+i*3,1+i*3,2+i*3
+    
+    listall1.append([titles[i].text,hrefs[i].get_attribute('href'),imgs[i].get_attribute('src'),Popularitys[x].text.split('\n')[0],Popularitys[x].text.split('\n')[1],Popularitys[y].text.replace(' ',':',1),Popularitys[z].text.replace('\n',':',1).replace('\n',"")])
+
+print(listall1)
