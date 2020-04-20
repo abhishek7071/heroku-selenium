0a98ecebaacacfceb40a2be490c632f02dc7c529
henry <q355261@gmail.com>
2020-04-20 22:27:35 +0800

    set 10:31

--- henry_selenium.py
+++ henry_selenium.py
@@ -47,7 +47,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22)
+#@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22)
+@sched.cron_schedule(day_of_week='mon-fri', hour=22,minute=31)
 def timed_job():   
     url = 'https://today.line.me/TW/publisher/101508'
     df4 = pandas.DataFrame(list(parse_source(url)))
