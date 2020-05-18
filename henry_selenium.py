33b16a2ecdfdaa98ebc4cecf0f5476102bde8830
henry8082 <56972220+henry8082@users.noreply.github.com>
2020-05-18 20:06:17 +0800

    Update henry_selenium.py

--- henry_selenium.py
+++ henry_selenium.py
@@ -60,7 +60,7 @@ def timed_job():
         time_s = str(df4["time"]).split(' ')[0] 
         now = datetime.datetime.now()   
         if now_s == time_s:
-            token = 'kEujm1BoMS0AvgNnF8QQ8RHXbUgO3viRsI3oFrZNhTT'
+            token = 'Line Notify所給的token'
             headers = {
                 'Content-type': 'application/x-www-form-urlencoded',
                 'Authorization': f'Bearer {token}'
@@ -85,29 +85,3 @@ sched.add_job(timed_job, 'cron', day_of_week='mon-fri', hour=19,minute = 1)
 
 sched.start()
 
-
-
-
-
-
-
-
-#driver = webdriver.Chrome(r"C:\Users\user\Desktop\聯成助教資料\練習\chromedriver")
-# url = "https://fuli.gamer.com.tw/shop.php"
-
-# driver.get(url)
-# #selenium-使用 https://jzchangmark.wordpress.com/2015/03/16/selenium-%E4%BD%BF%E7%94%A8-css-locator-%E5%AE%9A%E4%BD%8D%E5%85%83%E4%BB%B6/
-# titles = driver.find_elements_by_class_name('items-title')
-# hrefs = driver.find_elements_by_css_selector("a[class=items-card][href^='https']")#找尋tag為a,class為items-card,href的開頭為https
-# imgs = driver.find_elements_by_css_selector("div.card-left.flex-center > img")
-# Popularitys = driver.find_elements_by_css_selector("div.card-right div.items-instructions")
-
-# listall1 = []
-# for i in range(len(titles)):
-#     x,y,z=0+i*3,1+i*3,2+i*3
-    
-#     listall1.append([titles[i].text,hrefs[i].get_attribute('href'),imgs[i].get_attribute('src'),Popularitys[x].text.split('\n')[0],Popularitys[x].text.split('\n')[1],Popularitys[y].text.replace(' ',':',1),Popularitys[z].text.replace('\n',':',1).replace('\n',"")])
-
-# print(listall1)
-
-# df1 =pandas.DataFrame(listall1,columns=["title","href","img","人氣","商品數量","活動時間","購買方式"])
